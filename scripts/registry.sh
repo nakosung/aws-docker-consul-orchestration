b9f8913fb1e2d5bbf42b487a6fd36367be502d15
Nako Sung <deif79@gmail.com>
2014-06-13 08:23:06 +0900

    fixed docker registry check script

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -1,6 +1,6 @@
 #include worker.sh
 
 sudo docker run --name registry -p 80:5000 -d registry
-echo '{"service":{"name":"docker","port":80,"check":{"script":"nc -z localhost 5000","interval":"10s"}}}' | sudo tee -a /etc/consul.d/registry.json
+echo '{"service":{"name":"docker","port":80,"check":{"script":"nc -z localhost 80","interval":"10s"}}}' | sudo tee -a /etc/consul.d/registry.json
 
 sudo killall -s 1 consul
