397b45b14fae133788b2e44330c33f62357a67a1
Nako Sung <deif79@gmail.com>
2014-06-16 00:02:39 +0900

    no more local dns

--- scripts/docker_runner.sh
+++ scripts/docker_runner.sh
@@ -1,12 +1,16 @@
 #include worker.sh
+
+mkdir -p /home/ubuntu
+cd /home/ubuntu
+
 cat > lookup.sh << 'END_LOOKUP_SH'
-echo tcp://$(dig +short $1.service.consul):$(dig +short $1.service.consul SRV | awk '{print $3}')
+echo tcp://$(dig @localhost -p 8600 +short $1.service.consul):$(dig @localhost -p 8600 +short $1.service.consul SRV | awk '{print $3}')
 sudo killall -s 1 consul
 END_LOOKUP_SH
 chmod +x lookup.sh
 
 cat > run.sh << 'END_RUN_SH'
-sudo docker run -e REDIS_PORT=$(./lookup.sh redis) -e ZK_PORT=$(./lookup.sh zookeeper) -e MONGO_PORT=$(./lookup.sh mongo) --net=host -d $(dig +short docker.service.consul)/$1
+sudo docker run -e REDIS_PORT=$(./lookup.sh redis) -e ZK_PORT=$(./lookup.sh zookeeper) -e MONGO_PORT=$(./lookup.sh mongo) --net=host -d $(dig @localhost -p 8600 +short docker.service.consul)/$1
 END_RUN_SH
 chmod +x run.sh
 
